ce778ad4b4ce35f51e8e87fa4ec359932362cba1
Beverly Hill <bjsrhill@gmail.com>
2023-08-08 15:34:13 -0500

    Update ORKTypes.h
    Removed if target iOS from Foundation since it was just a change test.

--- ResearchKit/Common/ORKTypes.h
+++ ResearchKit/Common/ORKTypes.h
@@ -28,9 +28,8 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#if TARGET_OS_IOS
-    @import Foundation;
-#endif
+@import Foundation;
+
 #if TARGET_OS_IOS
     @import UIKit;
 #endif
